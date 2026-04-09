d65b781a5ac4d6e74b24376b61be038fdec623e7
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-09 11:15:51 +0300

    src klasoru icindeki app.jsx dosyasinda form elementi icinde kullanicinin ismini girecegi alan olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ export default function App() {
     <>
       <form action="">
         <h1>İletişim Formu</h1>
+        <div className='user-information'>
+          <h4>Adınız</h4>
+          <input type="text" placeholder='Adınızı Giriniz' />
+        </div>
       </form>
     </>
   )
