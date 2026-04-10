575a6821ba3d45cc74d461a331ee07d4d24e80f8
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-10 12:40:39 +0300

    src klasoru icindeki assets klasoru icindeki components klasoru icindeki contactform.jsx component'inda mobil gorunum icin success message alani olusturuldu, css duzenlemesi yapildi

--- src/assets/Components/ContactForm.jsx
+++ src/assets/Components/ContactForm.jsx
@@ -1,50 +1,74 @@
+import { useEffect, useState } from 'react'
 import '../../App'
 
 export default function ContactForm() {
+
+  const [submit, setSubmit] = useState(false);
+
+  // Gonder Butonuna Tiklandiginda Yapilacak Islem
+  useEffect(() => {
+    if (submit) {
+      console.log("Mesaj Gönderildi");
+    }
+  }, [submit])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmit(true);
+  }
+
   return (
     <>
       <div className='out-color'>
-        <form action="">
-          <div className='in-color'>
-            <h1>İletişim Formu</h1>
-            <div className="firstname-and-lastname">
-              <div className='user-information'>
-                <h4>Adınız</h4>
-                <input type="text" placeholder='Adınızı Giriniz' />
+        <div className="form-wrapper">
+          {submit && (
+            <div className='success-message'>
+              <h1>Mesaj Gönderildi</h1>
+              <h4>Formu doldurduğunuz için teşekkür ederiz. Yakında sizinle iletişime geçeceğiz!</h4>
+            </div>
+          )}
+          <form onSubmit={handleSubmit}>
+            <div className='in-color'>
+              <h1>İletişim Formu</h1>
+              <div className="firstname-and-lastname">
+                <div className='user-information'>
+                  <h4>Adınız</h4>
+                  <input type="text" placeholder='Adınızı Giriniz' />
+                </div>
+                <div className='user-information'>
+                  <h4>Soyadınız</h4>
+                  <input type="text" placeholder='Soyadınızı Giriniz' />
+                </div>
               </div>
               <div className='user-information'>
-                <h4>Soyadınız</h4>
-                <input type="text" placeholder='Soyadınızı Giriniz' />
+                <h4>E-Mail Adresiniz</h4>
+                <input type="text" placeholder='E-Mail Adresiniz Giriniz' />
               </div>
-            </div>
-            <div className='user-information'>
-              <h4>E-Mail Adresiniz</h4>
-              <input type="text" placeholder='E-Mail Adresiniz Giriniz' />
-            </div>
-            <div className='query-type'>
-              <h4>Sorgu Türü</h4>
-              <div className="query-options">
-                <h4 className='query-types'>
-                  <input type="radio" />
-                  Genel Sorular
-                </h4>
-                <h4 className='query-types'>
-                  <input type="radio" />
-                  Destek Talebi
-                </h4>
+              <div className='query-type'>
+                <h4>Sorgu Türü</h4>
+                <div className="query-options">
+                  <h4 className='query-types'>
+                    <input type="radio" />
+                    Genel Sorular
+                  </h4>
+                  <h4 className='query-types'>
+                    <input type="radio" />
+                    Destek Talebi
+                  </h4>
+                </div>
               </div>
+              <div className='message'>
+                <h4>Mesaj</h4>
+                <textarea name="" id="" rows={10}></textarea>
+              </div>
+              <div className='check'>
+                <input type="checkbox" name="" id="" />
+                <h4>Ekip tarafından benimle iletişime geçilmesini kabul ediyorum *</h4>
+              </div>
+              <button type='submit'>Gönder</button>
             </div>
-            <div className='message'>
-              <h4>Mesaj</h4>
-              <textarea name="" id="" rows={10}></textarea>
-            </div>
-            <div className='check'>
-              <input type="checkbox" name="" id="" />
-              <h4>Ekip tarafından benimle iletişime geçilmesini kabul ediyorum *</h4>
-            </div>
-            <button>Gönder</button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </>
   )
